2f786f56ec93931684d411a7ef8fb5a71e494dd9
kalio007 <kalioprince1@gmail.com>
2024-01-22 14:53:33 +0100

    feat:completed the User routes

--- main.go
+++ main.go
@@ -5,15 +5,34 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kalio007/Fiber-Gorm-API/database"
+	"github.com/kalio007/Fiber-Gorm-API/routes"
 )
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to this Awesome API")
 }
+func setupRoutes(app *fiber.App) {
+	// Welcome endpoint
+	app.Get("/api", welcome)
+	// User endpoints
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
+	// Product endpoints
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
+	app.Put("/api/products/:id", routes.UpdateProduct)
+	// Order endpoints
+	app.Post("/api/orders", routes.CreateOrder)
+	app.Get("/api/orders", routes.GetOrders)
+	app.Get("/api/orders/:id", routes.GetOrder)
+}
 
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
-	app.Get("/api", welcome)
+	setupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
 }
